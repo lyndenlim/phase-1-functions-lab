4563ef87ec857839ac7a09754df156884661efe2
Lynden Lim <lyndenlim15@gmail.com>
2022-01-10 12:40:49 -0500

    commit tree 7c7c07865022732d90dcf26504e84fbd679a229d

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distance){
+    if (distance >= 42) {
+        return (distance - 42);
+    } else {
+        return (distance - 26);
+    };
+};
+
+function distanceFromHqInFeet(distance){
+    return Math.abs(distance - 42) * 264;
+};
+
+function distanceTravelledInFeet(x,y){
+    if (x > y){
+        return (((x - 42) * 264) - ((y - 42) * 264));
+    } else {
+        return ((Math.abs(y - 42) * 264) - (Math.abs(x - 42) * 264));
+    };
+};
+
+function calculatesFarePrice(x,y){
+    if (Math.abs((x - y)) <= 1){
+        return 0;
+     } else if (Math.abs(x - y) > 1 && Math.abs(x - y) < 7) {
+        return ((((x - y) * 264) - 400) * 0.02)
+     } else if (Math.abs(x - y) > 7 && Math.abs(x - y) < 9) {
+         return 25;
+     } else {
+         return "cannot travel that far";
+     };
+};
